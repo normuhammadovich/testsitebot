13137dcc91e4a497579d29a2aa1be21d831a97ac
Chingiz <100722958+normuhammadovich@users.noreply.github.com>
2024-12-16 23:51:53 +0500

    Update app.js

--- site/app.js
+++ site/app.js
@@ -80,7 +80,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server http://localhost:${PORT} da ishga tushdi (Webhook uchun HTTPS kerak!)`);
 });
